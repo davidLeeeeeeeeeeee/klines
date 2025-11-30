4a26d3ada349f5370def5907823f0ed381906f0d
davidleeeeee <exmple@email.com>
2025-11-30 19:12:29 +0800

    14

--- src/router/index.js
+++ src/router/index.js
@@ -1,4 +1,4 @@
-import { createRouter, createWebHistory } from 'vue-router'
+import { createRouter, createWebHashHistory } from 'vue-router'
 import { isAuthenticated } from '../utils/auth'
 
 // 导入组件
@@ -59,7 +59,7 @@ const routes = [
 ]
 
 const router = createRouter({
-  history: createWebHistory(),
+  history: createWebHashHistory(),
   routes
 })
 
